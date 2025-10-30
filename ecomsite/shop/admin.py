492478933eec4230d8c1f2864245f7cfd2f382e6
Uddin Nahim <nahim.211902019@gmail.com>
2025-10-31 02:06:44 +0600

    Updating Headers

--- ecomsite/shop/admin.py
+++ ecomsite/shop/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Products,Order
 
+admin.site.site_header ="E-commerce site"
+admin.site.site_title = "ABC Shopping"
+admin.site.index_title = "Manage ABC Shopping"
+
 admin.site.register(Products)
 admin.site.register(Order)
 
